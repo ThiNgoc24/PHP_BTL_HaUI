86e6c8ec7ed29aec326bd40a03e9cd9634d88502
ManhNguyen <nguyenmanh14092003@gmail.com>
2024-06-25 23:53:19 +0700

    update

--- Admin/LOGIN/index.php
+++ Admin/LOGIN/index.php
@@ -1,3 +1,3 @@
 <?php
-echo "helooo";
+echo "Đây là trang chủ nhá ";
 ?>
--- Admin/LOGIN/login_functions.php
+++ Admin/LOGIN/login_functions.php
@@ -1,7 +1,7 @@
 <?php
 // Khởi tạo phiên
 session_start();
-include "../conn_db.php";
+include "../../conn_db.php";
 
 $username_error = "";
 $password_error = "";
@@ -22,29 +22,32 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['btn_login'])) {
     }
    
     // Nếu không có lỗi, tiến hành kiểm tra đăng nhập
-    if (empty($username_error) && empty($password_error)) {
-        if ($role === 'user') {
-            $table = 'member';
-        } elseif ($role === 'admin') {
-            $table = 'admin';
-        } else {
-            // Xử lý trường hợp không xác định, tuy frontend nên xác thực lựa chọn này
-            // Có thể chuyển hướng hoặc hiển thị thông báo lỗi
-        }
-
-        // Kiểm tra thông tin đăng nhập
-        $sql = "SELECT * FROM $table WHERE username='$username' AND password='$password'";
-        $result = queryDatabase($sql);
-        
-        if (!empty($result)) {
-            $_SESSION['id'] = $result['id'];
-            $_SESSION['username'] = $username;
-            header("Location: index.php"); // Chuyển hướng đến trang chính sau khi đăng nhập thành công
-            exit();
-        } else {
-            $password_error = "Tên đăng nhập hoặc mật khẩu không chính xác.";
-        }
+if (empty($username_error) && empty($password_error)) {
+    if ($role === 'user') {
+        $table = 'member';
+        $redirect_page = 'index.php'; // Trang chuyển hướng cho vai trò user
+    } elseif ($role === 'admin') {
+        $table = 'admin';
+        $redirect_page = '../MANAGE_INDEX/Manage_index_page.php'; // Trang chuyển hướng cho vai trò admin
+    } else {
+        // Xử lý trường hợp không xác định, tuy frontend nên xác thực lựa chọn này
+        // Có thể chuyển hướng hoặc hiển thị thông báo lỗi
     }
+
+    // Kiểm tra thông tin đăng nhập
+    $sql = "SELECT * FROM $table WHERE username='$username' AND password='$password'";
+    $result = queryDatabase($sql);
+    
+    if (!empty($result)) {
+        $_SESSION['id'] = $result['id'];
+        $_SESSION['username'] = $username;
+        header("Location: $redirect_page"); // Chuyển hướng đến trang tương ứng với vai trò
+        exit();
+    } else {
+        $password_error = "Tên đăng nhập hoặc mật khẩu không chính xác.";
+    }
+}
+
 }
 
 include "login_page.php";
